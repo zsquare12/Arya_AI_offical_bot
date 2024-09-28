f9a9273bd75f324132e811fd99d67e6d75e4ab19
Himanshu Gangwar <151658698+zenitsu0509@users.noreply.github.com>
2024-09-28 10:13:03 +0000

    i did it

---
+++ main.py
@@ -0,0 +1,82 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+import os
+from langchain.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+from langchain.llms import HuggingFaceHub
+from huggingface_hub import InferenceClient
+from langchain import PromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
+from langchain.llms import HuggingFaceHub
+
+load_dotenv()
+loader = TextLoader('data/hostel_data.txt')
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=4)
+docs = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings()
+
+pinecone.init(
+    api_key= os.getenv('API_KEY_PINECONE'), 
+    environment='gcp-starter'
+)
+
+# Define Index Name
+index_name = "langchain-demo"
+
+if index_name not in pinecone.list_indexes():
+  
+  pinecone.create_index(name=index_name, metric="cosine", dimension=768)
+  docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+else:
+  
+  docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+
+
+template = """
+You are a Arya a bot of hostel. These Human will ask you a questions about the hostel. 
+Use following piece of context to answer the question. 
+If you don't know the answer, just say you don't know. 
+Keep the answer within 1 sentences and concise.
+
+
+Question: {question}
+Answer: 
+
+"""
+
+prompt = PromptTemplate(
+  template=template, 
+  input_variables=["context", "question"]
+)
+llm = HuggingFaceHub(
+    repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1", 
+    huggingfacehub_api_token=os.getenv("HUGGING_FACE_API") 
+)
+
+
+rag_chain = (
+  {"context": docsearch.as_retriever(),  "question": RunnablePassthrough()} 
+  | prompt 
+  | llm 
+  | StrOutputParser() 
+)
+class ChatBot():
+  loader = TextLoader('data/hostel_data.txt')
+  documents = loader.load()
+  rag_chain = (
+    {"context": docsearch.as_retriever(),  "question": RunnablePassthrough()} 
+    | prompt 
+    | llm
+    | StrOutputParser() 
+  )
+
+bot = ChatBot()
+user_input = input("Ask me anything: ")
+result = bot.rag_chain.invoke(user_input)
+print(result)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,35 @@
+from main import ChatBot
 import streamlit as st
-import pandas as pd
-import math
-from pathlib import Path
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+bot = ChatBot()
+    
+st.set_page_config(page_title="ARYA-AI (The AryaBhatt offical bot)")
+with st.sidebar:
+    st.title('ARYA-AI (The AryaBhatt offical bot)')
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+def generate_response(input):
+    result = bot.rag_chain.invoke(input)
+    return result
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome, To the Arya_Bhatt Offical Bot"}]
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-st.header(f'GDP in {to_year}', divider='gray')
 
-''
+if input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": input})
+    with st.chat_message("user"):
+        st.write(input)
 
-cols = st.columns(4)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Getting your answer from from Data Base.."):
+            response = generate_response(input) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
